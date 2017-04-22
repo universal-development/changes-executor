0acd00946fb1a0c2241a65a768c1252e4c39963a
Denis O <denis.o@linux.com>
2017-04-22 14:44:38 +0300

    Added constructors arguments

--- changes-executor-core/src/main/java/com/unidev/changesexecutor/core/ChangeExecutionException.java
+++ changes-executor-core/src/main/java/com/unidev/changesexecutor/core/ChangeExecutionException.java
@@ -4,4 +4,20 @@ package com.unidev.changesexecutor.core;
  * Exception thrown if change execution fails
  */
 public class ChangeExecutionException extends RuntimeException {
+
+    public ChangeExecutionException() {
+        super();
+    }
+
+    public ChangeExecutionException(String message) {
+        super(message);
+    }
+
+    public ChangeExecutionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ChangeExecutionException(Throwable cause) {
+        super(cause);
+    }
 }
